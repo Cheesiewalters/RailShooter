dfd8291d8faea9593e3f2f4bda557b19d774526c
Conor Walters <cwalters01@qub.ac.uk>
2020-01-03 13:32:09 +0000

    Adding assets and creating Terrain

---
+++ Assets/SplahScreen.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SplahScreen : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Invoke("LoadLevel", 5f);
+    }
+    
+    void LoadLevel()
+    {
+        SceneManager.LoadScene(1);
+    }
+    
+}
