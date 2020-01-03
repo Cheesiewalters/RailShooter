78d16593dee5638d885f484467245f9d4caed8bf
Conor Walters <cwalters01@qub.ac.uk>
2020-01-03 15:52:37 +0000

    Adding player Script and Standard Unity assets Folder

---
+++ Assets/Player.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class Player : MonoBehaviour
+{
+    [Tooltip("In meters/s")][SerializeField]
+    private float playervelocity = 100f;
+    [Tooltip("In m")] [SerializeField] 
+    float xRange = 10f;
+    void Start()
+    {
+       
+    }
+
+    void Update()
+    {
+        processTranslation();
+        processRotation();
+    }
+
+    private void processRotation()
+    {
+        transform.localRotation = Quaternion.Euler(0f, 90f, 0f);
+    }
+
+    private void processTranslation()
+    {
+        float horizontalThrow = CrossPlatformInputManager.GetAxis("Horizontal");
+        float verticalThrow = CrossPlatformInputManager.GetAxis("Vertical");
+
+        float xOffset = horizontalThrow * playervelocity * Time.deltaTime;
+        float yOffset = verticalThrow * playervelocity * Time.deltaTime;
+
+        float rawXPos = transform.localPosition.x + xOffset;
+        float rawyPos = transform.localPosition.y + yOffset;
+
+        float clampedXPos = Mathf.Clamp(rawXPos, -xRange, xRange);
+        float clampedYPos = Mathf.Clamp(rawyPos, -xRange, xRange);
+
+        transform.localPosition = new Vector3(clampedXPos, clampedYPos, transform.localPosition.z);
+    }
+}
--- Assets/SplahScreen.cs
+++ Assets/SplahScreen.cs
@@ -1,10 +1,16 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class SplahScreen : MonoBehaviour
 {
+    private void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+
     // Start is called before the first frame update
     void Start()
     {
